ff4906dcfe533e9b87dd3c51ec5f48ba9ad86bea
Anthony Thudium <thudius.anthony@gmail.com>
2017-06-03 11:39:10 -0700

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,8 @@
+#python3 pizza.py
+
+def make_pizza(size, *toppings):
+	"""Summarize the pizza we are about to make."""
+	print("\nMaking a " + str(size) + 
+				"- inch pizza with the following toppings:")
+	for topping in toppings:
+		print("\t- " + topping.title())
